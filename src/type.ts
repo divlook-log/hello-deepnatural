5c256c64b55afd2017dfb1d6997587c60bc25c91
uihyeon.cha <uihyeon.cha@gmail.com>
2021-02-08 23:24:13 +0900

    데이터에 SelectableLabel 추가

--- src/type.ts
+++ src/type.ts
@@ -5,8 +5,14 @@ export interface Messages {
     createdAt: string
 }
 
+export interface SelectableLabel {
+    topic: string
+    description: string
+    important: boolean
+}
+
 export interface TaskAssign {
     title: string
     body: string
-    messages: Messages[]
+    selectableLabels: SelectableLabel[]
 }
