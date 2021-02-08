68eeac438ef1668f29c1945a556768f3e916d8b1
uihyeon.cha <uihyeon.cha@gmail.com>
2021-02-08 22:16:15 +0900

    LabelingTool 컴포넌트 중간 저장

--- src/views/LabelingTool.vue
+++ src/views/LabelingTool.vue
@@ -1,14 +1,99 @@
 <template>
-    <div class="LabelingTool"></div>
+    <div class="LabelingTool">
+        <div class="Panels">
+            <!-- Left -->
+            <div class="PanelItem Panel-Left">
+                <div class="card">
+                    <div class="card-header">
+                        <sub>
+                            <strong>TASK Instruction</strong>
+                        </sub>
+                        <br />
+                        <h3>
+                            <strong>{{ info.title || '...' }}</strong>
+                        </h3>
+                    </div>
+
+                    <div class="card-body">
+                        {{ info.body }}
+                    </div>
+                </div>
+
+                <div class="card">
+                    <div class="card-body">
+                        <h3>
+                            <strong>Good Job!</strong>
+                        </h3>
+                        I think AI will be smart soon :)<br />
+                        <br />
+                        <DNProgressBar :completed="43" :total="51" />
+                    </div>
+                </div>
+            </div>
+
+            <!-- Center -->
+            <div class="PanelItem Panel-Center">
+                <div class="card">
+                    <div class="card-body">
+                        <template v-for="msg in info.messages">
+                            <DNMessage
+                                :key="msg.id"
+                                :id="msg.id"
+                                :body="msg.body"
+                                :created-at="msg.createdAt"
+                                :my-msg="msg.isMyMsg"
+                            />
+                        </template>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 </template>
 
 <script lang="ts">
 import { Component, Vue } from 'vue-property-decorator'
+import DNProgressBar from '@/components/DNProgressBar.vue'
+import DNMessage from '@/components/DNMessage.vue'
+import { TaskAssign } from '@/type'
 
 @Component({
-    components: {},
+    components: {
+        DNProgressBar,
+        DNMessage,
+    },
 })
-export default class LabelingTool extends Vue {}
+export default class LabelingTool extends Vue {
+    info: TaskAssign = {
+        title: '',
+        body: '',
+        messages: [],
+    }
+
+    // - 화면 좌측 패널의 작업 가이드 내용 (제목과 본문)
+    // - 화면 중앙의 대화 내용
+    // - 화면 우측 패널의 내용 (패널의 제목, 설명, 그리고 레이블 내용)
+
+    mounted() {
+        this.getTaskAssignApi()
+    }
+
+    async getTaskAssignApi() {
+        try {
+            const res = await fetch('/api/task-assign.json')
+
+            if (!res.ok) {
+                throw new Error('Network response was not ok.')
+            }
+
+            const data: TaskAssign = await res.json()
+
+            this.info = data
+        } catch (error) {
+            console.error(error)
+        }
+    }
+}
 </script>
 
 <style lang="scss" scoped>
@@ -17,5 +102,27 @@ export default class LabelingTool extends Vue {}
 .LabelingTool {
     background-color: $color-steel-gray;
     height: 100%;
+
+    .Panels {
+        padding: 10px;
+        display: flex;
+        flex-flow: wrap;
+
+        .PanelItem {
+            margin-right: 10px;
+
+            & > .card {
+                margin-bottom: 10px;
+            }
+
+            &.Panel-Left {
+                max-width: 340px;
+            }
+
+            &.Panel-Center {
+                min-width: 630px;
+            }
+        }
+    }
 }
 </style>
